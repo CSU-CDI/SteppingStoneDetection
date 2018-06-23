bab578dc85f1b4fb25d488e989b160a75462cd69
Lock <Lock@LAPTOP-4SHU10J1>
2018-06-23 00:05:01 -0400

    added ecrossover packet form

---
+++ SteppingStoneCapture/Analysis/CrossOverPacket/CrossOverPacket.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace SteppingStoneCapture.Analysis
+{
+    partial class CrossOverPacket
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CrossOverPacket));
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnBrowseOutput = new System.Windows.Forms.Button();
+            this.btnBrowseInput = new System.Windows.Forms.Button();
+            this.txtOutputStream = new System.Windows.Forms.TextBox();
+            this.txtInputStream = new System.Windows.Forms.TextBox();
+            this.lblOutputStream = new System.Windows.Forms.Label();
+            this.lblInputStream = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Threshold:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 257);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 16;
+            // 
+            // btnBrowseOutput
+            // 
+            this.btnBrowseOutput.Location = new System.Drawing.Point(13, 164);
+            this.btnBrowseOutput.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrowseOutput.Name = "btnBrowseOutput";
+            this.btnBrowseOutput.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowseOutput.TabIndex = 15;
+            this.btnBrowseOutput.Text = "Browse...";
+            this.btnBrowseOutput.UseVisualStyleBackColor = true;
+            // 
+            // btnBrowseInput
+            // 
+            this.btnBrowseInput.Location = new System.Drawing.Point(13, 76);
+            this.btnBrowseInput.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrowseInput.Name = "btnBrowseInput";
+            this.btnBrowseInput.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowseInput.TabIndex = 14;
+            this.btnBrowseInput.Text = "Browse...";
+            this.btnBrowseInput.UseVisualStyleBackColor = true;
+            // 
+            // txtOutputStream
+            // 
+            this.txtOutputStream.Location = new System.Drawing.Point(13, 134);
+            this.txtOutputStream.Margin = new System.Windows.Forms.Padding(4);
+            this.txtOutputStream.Name = "txtOutputStream";
+            this.txtOutputStream.Size = new System.Drawing.Size(424, 22);
+            this.txtOutputStream.TabIndex = 13;
+            // 
+            // txtInputStream
+            // 
+            this.txtInputStream.Location = new System.Drawing.Point(13, 46);
+            this.txtInputStream.Margin = new System.Windows.Forms.Padding(4);
+            this.txtInputStream.Name = "txtInputStream";
+            this.txtInputStream.Size = new System.Drawing.Size(424, 22);
+            this.txtInputStream.TabIndex = 12;
+            // 
+            // lblOutputStream
+            // 
+            this.lblOutputStream.AutoSize = true;
+            this.lblOutputStream.Location = new System.Drawing.Point(10, 113);
+            this.lblOutputStream.Name = "lblOutputStream";
+            this.lblOutputStream.Size = new System.Drawing.Size(217, 17);
+            this.lblOutputStream.TabIndex = 11;
+            this.lblOutputStream.Text = "Select Output Stream From File...";
+            // 
+            // lblInputStream
+            // 
+            this.lblInputStream.AutoSize = true;
+            this.lblInputStream.Location = new System.Drawing.Point(10, 25);
+            this.lblInputStream.Name = "lblInputStream";
+            this.lblInputStream.Size = new System.Drawing.Size(205, 17);
+            this.lblInputStream.TabIndex = 10;
+            this.lblInputStream.Text = "Select Input Stream From File...";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(194, 248);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(113, 39);
+            this.btnOk.TabIndex = 19;
+            this.btnOk.Text = "&Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(323, 248);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(113, 39);
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // CrossOverPacket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 309);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnBrowseOutput);
+            this.Controls.Add(this.btnBrowseInput);
+            this.Controls.Add(this.txtOutputStream);
+            this.Controls.Add(this.txtInputStream);
+            this.Controls.Add(this.lblOutputStream);
+            this.Controls.Add(this.lblInputStream);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "CrossOverPacket";
+            this.Text = "Cross-Over Packet Detection";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnBrowseOutput;
+        private System.Windows.Forms.Button btnBrowseInput;
+        private System.Windows.Forms.TextBox txtOutputStream;
+        private System.Windows.Forms.TextBox txtInputStream;
+        private System.Windows.Forms.Label lblOutputStream;
+        private System.Windows.Forms.Label lblInputStream;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ SteppingStoneCapture/Analysis/CrossOverPacket/CrossOverPacket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteppingStoneCapture.Analysis
+{
+    public partial class CrossOverPacket : Form
+    {
+        public CrossOverPacket()
+        {
+            InitializeComponent();
+            btnOk.Enabled = false;
+        }
+    }
+}
